97440cf526a4d94dd1166607d04bcc8a87f19bf8
sony saini <100938017+sonysaini123@users.noreply.github.com>
2026-05-08 21:38:21 +0530

    Update IsPalindrome.py
    Added palindrome number problem solution

--- IsPalindrome.py
+++ IsPalindrome.py
@@ -1,1 +1,15 @@
-Added palindrome number problem solution
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        temp = x
+        reverse=0
+        if temp<0:
+            return False
+        while temp>0:
+            rem=temp%10
+            temp//=10
+            reverse=(reverse*10)+rem
+        return reverse==x
+
+#short method- using by string
+       
+        #return str(x)==str(x)[::-1]
